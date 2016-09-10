c7f80f48cb2dd01a4a071db97e60ca1eff6757c5
Stuckinaboot <Stuckinaboot@levyvlan502.0937.wlan.wireless-resnet.upenn.edu>
2016-09-09 20:52:59 -0400

    Added iOS and some iOS system design ideas

---
+++ iOS/MapInside/MapInside/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapInside
+//
+//  Created by Stuckinaboot on 9/9/16.
+//  Copyright © 2016 Stuckinaboot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
