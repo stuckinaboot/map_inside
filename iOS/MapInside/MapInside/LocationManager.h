173a9693b20805992cba6896fdc77a1890264f00
Stuckinaboot <Stuckinaboot@seas.0251.apng.wireless-pennnet.upenn.edu>
2016-09-11 03:08:53 -0400

    Most recent

---
+++ iOS/MapInside/MapInside/LocationManager.h
@@ -0,0 +1,13 @@
+//
+//  LocationManager.h
+//  MapInside
+//
+//  Created by Stuckinaboot on 9/11/16.
+//  Copyright © 2016 Stuckinaboot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationManager : NSObject
+
+@end
---
+++ iOS/MapInside/MapInside/LocationManager.m
@@ -0,0 +1,13 @@
+//
+//  LocationManager.m
+//  MapInside
+//
+//  Created by Stuckinaboot on 9/11/16.
+//  Copyright © 2016 Stuckinaboot. All rights reserved.
+//
+
+#import "LocationManager.h"
+
+@implementation LocationManager
+
+@end
