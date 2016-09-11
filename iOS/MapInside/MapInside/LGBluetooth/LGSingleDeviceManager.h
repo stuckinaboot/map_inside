23584f1f8fe1ef819319d5911b14ba5f28d62261
Stuckinaboot <Stuckinaboot@seas0450.wlan.seas.upenn.edu>
2016-09-10 22:28:03 -0400

    Pre-making interface nicer and adding bluetooth support

---
+++ iOS/MapInside/MapInside/LGBluetooth/LGSingleDeviceManager.h
@@ -0,0 +1,20 @@
+//
+//  LGSingleDeviceManager.h
+//  LGBluetoothExample
+//
+//  Created by Stuckinaboot on 9/10/16.
+//  Copyright © 2016 David Sahakyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "LGBluetooth.h"
+
+@interface LGSingleDeviceManager : NSObject {
+    LGPeripheral *device;
+}
+- (void)searchForDevice:(NSString*)deviceName
+             completion:(void (^_Nonnull)(BOOL deviceFound))completionHandler;
+- (void)subscribeToNotificationsForService:(NSString*)serviceName
+                            characteristic:(NSString*)characteristicName
+                                onUpdate:(void (^_Nonnull)(NSData *data, NSError *error))updateHandler;
+@end
---
+++ iOS/MapInside/MapInside/LGBluetooth/LGSingleDeviceManager.m
@@ -0,0 +1,63 @@
+//
+//  LGSingleDeviceManager.m
+//  LGBluetoothExample
+//
+//  Created by Stuckinaboot on 9/10/16.
+//  Copyright © 2016 David Sahakyan. All rights reserved.
+//
+
+#import "LGSingleDeviceManager.h"
+
+@implementation LGSingleDeviceManager
+
+- (id)init {
+    if (self = [super init]) {
+        [LGCentralManager sharedInstance];
+    }
+    return self;
+}
+
+- (void)searchForDevice:(NSString*)deviceName
+             completion:(void (^_Nonnull)(BOOL deviceFound))completionHandler {
+    [[LGCentralManager sharedInstance] scanForPeripheralsByInterval:4
+                                                         completion:^(NSArray *peripherals)
+     {
+         device = NULL;
+         if (peripherals.count) {
+             for (LGPeripheral *peripheral in peripherals) {
+                 if ([peripheral.advertisingData[@"kCBAdvDataLocalName"] isEqualToString:deviceName]) {
+                     device = peripheral;
+                     break;
+                 }
+             }
+         }
+
+         completionHandler(device != NULL);
+     }];
+}
+
+- (void)subscribeToNotificationsForService:(NSString*)serviceName
+                            characteristic:(NSString*)characteristicName
+                                  onUpdate:(void (^ _Nonnull)(NSData *, NSError *))updateHandler {
+    [device connectWithCompletion:^(NSError *error) {
+        [device discoverServicesWithCompletion:^(NSArray *services, NSError *error) {
+            for (LGService *service in services) {
+                if ([service.UUIDString isEqualToString:serviceName]) {
+                    [service discoverCharacteristicsWithCompletion:^(NSArray *characteristics, NSError *error) {
+                        for (LGCharacteristic *charact in characteristics) {
+                            if ([charact.UUIDString isEqualToString:characteristicName]) {
+                                [charact setNotifyValue:YES completion:nil
+                                               onUpdate:^(NSData *data, NSError *error) {
+                                    updateHandler(data, error);
+                                }];
+                                break;
+                            }
+                        }
+                    }];
+                }
+            }
+        }];
+    }];
+}
+
+@end
